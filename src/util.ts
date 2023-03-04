e4c5fa921b05562536b517de6488e120e875b870
Frankie <1426203851@qq.com>
2023-03-04 19:05:16 +0800

    initial commit

---
+++ src/util.ts
@@ -0,0 +1,44 @@
+import * as vscode from 'vscode';
+
+export interface ISetting {
+  token: string
+  user: string
+  repo: string
+}
+
+export interface UrlSchema {
+  user: string
+  repo: string
+  file: string
+}
+
+export async function getSetting(): Promise<ISetting> {
+  const token = <string>await vscode.workspace.getConfiguration('aaa').get('github.token');
+  const user = <string>await vscode.workspace.getConfiguration('aaa').get('github.user');
+  const repo = <string>await vscode.workspace.getConfiguration('aaa').get('github.repo');
+  return { token, user, repo };
+}
+
+export const cdnURL = ({ user, repo, file }: UrlSchema) =>
+  `https://cdn.jsdelivr.net/gh/${user}/${repo}/${file}`;
+
+export async function to<T, U = Error>(
+  promise: Promise<T>,
+  errorExt?: object
+): Promise<[U | null, T | undefined]> {
+  try {
+    const data = await promise;
+    const result: [null, T] = [null, data];
+    console.log('data', data);
+    return result;
+  } catch (err) {
+    console.log('err', err);
+    if (errorExt) {
+      // @ts-ignore
+      Object.assign(err, errorExt);
+    }
+    // @ts-ignore
+    const resultWithError: [U, undefined] = [err, undefined];
+    return resultWithError;
+  }
+}
